16d29af9eb082db86c41aad5987c8d9d62dddbd3
hamza-halawa2020 <h.halawa2020@gmail.com>
2025-12-24 07:11:47 +0200

    up

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
     backEndUrl: 'https://backend.gorhom.com/api',
+    // backEndUrl: 'http://gorhom.test/api',
     imgUrl: 'https://backend.gorhom.com/',
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
     backEndUrl: 'https://backend.gorhom.com/api',
+    // backEndUrl: 'http://gorhom.test/api',
+
     imgUrl: 'https://backend.gorhom.com/',
 
 };
