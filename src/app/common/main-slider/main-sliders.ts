2129de9b5a4a2232f5b2d84ae568ed68d9e7f51e
hamza-halawa2020 <h.halawa2020@gmail.com>
2026-01-06 22:52:16 +0200

    feat: Add main slider data and corresponding image assets.

--- src/app/common/main-slider/main-sliders.ts
+++ src/app/common/main-slider/main-sliders.ts
@@ -2,11 +2,23 @@ export const MAIN_SLIDER_DATA = {
     data: [
         {
             id: 1,
-            image: 'assets/images/slider/1.jpeg',
+            image: 'assets/images/slider/1.jpg',
         },
         {
             id: 2,
-            image: 'assets/images/slider/2.jpeg',
+            image: 'assets/images/slider/2.jpg',
+        },
+        {
+            id: 3,
+            image: 'assets/images/slider/3.jpg',
+        },
+        {
+            id: 4,
+            image: 'assets/images/slider/4.jpg',
+        },
+        {
+            id: 5,
+            image: 'assets/images/slider/5.jpg',
         }
     ],
     status: 'success',
